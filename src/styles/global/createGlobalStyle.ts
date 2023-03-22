f57cf5af112633343f00a05c1cfbf8d60dc9df70
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-03-22 11:58:39 -0400

    feat: add tokens in global style

--- src/styles/global/createGlobalStyle.ts
+++ src/styles/global/createGlobalStyle.ts
@@ -4,7 +4,13 @@ import { ModernNormalize } from "./modern-normalize";
 import { Resets } from "./resets";
 import { Typography } from "./typography";
 
+import theme from "@/styles/theme";
+
 const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${theme.colors.background.primary};
+  }
+
   ${Typography}
   ${ModernNormalize}
   ${Resets}
--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -6,6 +6,27 @@ const breakpoints = {
 } as const;
 
 export const theme = {
+  colors: {
+    primary: {
+      main: "#009EDD",
+    },
+    secondary: {
+      main: "#039B00",
+    },
+    text: {
+      100: "#2F2E41",
+      200: "#999999",
+    },
+    auxiliar: {
+      100: "#D9D9D9",
+    },
+    background: {
+      primary: "#2F2E41",
+    },
+    base: {
+      white: "#FFFFFF",
+    },
+  },
   text: {
     family: {
       primary: "Open Sans, sans-serif",
